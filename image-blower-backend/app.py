a3054818cee82518e3d33824a08faf57a16e7f48
Antonin | Advert Ninja <antonin@advert.ninja>
2025-06-24 14:56:39 +0200

    num_block na 66, protože to pth očekává

--- image-blower-backend/app.py
+++ image-blower-backend/app.py
@@ -38,7 +38,7 @@ def load_model():
 
         # Inicializace základní architektury
         model_net = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
-                            num_block=23, num_grow_ch=32, scale=4)
+                            num_block=66, num_grow_ch=32, scale=4)
 
         model = RealESRGANer(
             scale=4,
